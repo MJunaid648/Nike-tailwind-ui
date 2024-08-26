6ea048bc931b8a1800c320243f11c0a63d2030ba
Muhammad Junaid <648mjunaid@gmail.com>
2024-08-26 12:25:49 +0500

    bare structure with tailwind config

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import SpecialOffers from "./SpecialOffers";
+import Services from "./Services";
+import CustomerReviews from "./CustomerReviews";
+import PopularProducts from "./PopularProducts";
+
+export {
+  Hero,
+  Services,
+  Footer,
+  Subscribe,
+  SuperQuality,
+  SpecialOffers,
+  CustomerReviews,
+  PopularProducts,
+};
