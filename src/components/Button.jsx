e5ebf72e2729f1ce664ba46f48c2025f23d939a7
Muhammad Junaid <648mjunaid@gmail.com>
2024-08-28 11:42:08 +0500

    little update to hero and button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,27 @@
-const Button = ({ label, iconURL }) => {
+const Button = ({
+  label,
+  iconURL,
+  backgroundColor,
+  textColor,
+  borderColor,
+}) => {
   return (
     <button
-      className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat
-                text-lg leading-none bg-coral-red rounded-full text-white border-coral-red"
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat
+                text-lg leading-none  rounded-full
+                 ${textColor ? textColor : "text-white"} 
+                 ${borderColor ? borderColor : "border-coral-red"}
+                 ${backgroundColor ? backgroundColor : "bg-coral-red"}
+                 `}
     >
       {label}
-      <img
-        src={iconURL}
-        alt="button icon"
-        className="ml-2 rounded-full w-5 h-5 "
-      />
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt="button icon"
+          className="ml-2 rounded-full w-5 h-5 "
+        />
+      )}
     </button>
   );
 };
