31da6ab8dcb98351f4932e937dc5b79636786089
Muhammad Junaid <648mjunaid@gmail.com>
2024-08-28 22:55:39 +0500

    complete

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -10,8 +10,10 @@ const Subscribe = () => {
         Sign Up For
         <span className="text-coral-red"> Updates</span> & Newsletter
       </h3>
-      <div className="lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border
-                       sm:border-slate-gray rounded-full">
+      <div
+        className="lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border
+                       sm:border-slate-gray rounded-full"
+      >
         <input type="text" placeholder="Subscribe@nike.com" className="input" />
         <div className="flex max-sm:justify-end items-center max-sm:w-full">
           <Button label="Sign Up" fullWidth />
